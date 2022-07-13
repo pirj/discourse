8a73ca1d8667d213047f7035d058a76cc6ef1086
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-07-13 15:43:02 +0400

    CLIENT: correct d-tooltip tests to match standard patterns

--- app/assets/javascripts/discourse/tests/integration/components/d-tooltip-test.js
+++ app/assets/javascripts/discourse/tests/integration/components/d-tooltip-test.js
@@ -1,53 +1,47 @@
-import { triggerEvent } from "@ember/test-helpers";
-import componentTest, {
-  setupRenderingTest,
-} from "discourse/tests/helpers/component-test";
+import { module, test } from "qunit";
+import { render, triggerEvent } from "@ember/test-helpers";
+import { setupRenderingTest } from "discourse/tests/helpers/component-test";
 import { hbs } from "ember-cli-htmlbars";
-import { discourseModule, query } from "discourse/tests/helpers/qunit-helpers";
+import { query } from "discourse/tests/helpers/qunit-helpers";
 
 async function mouseenter() {
   await triggerEvent(query("button"), "mouseenter");
 }
 
-discourseModule("Integration | Component | d-tooltip", function (hooks) {
+module("Integration | Component | d-tooltip", function (hooks) {
   setupRenderingTest(hooks);
 
-  componentTest("doesn't show tooltip if it wasn't expanded", {
-    template: hbs`
+  test("doesn't show tooltip if it wasn't expanded", async function (assert) {
+    await render(hbs`
       <button>
         <DTooltip>
           Tooltip text
         </DTooltip>
       </button>
-    `,
-
-    async test(assert) {
-      assert.notOk(document.querySelector("[data-tippy-root]"));
-    },
+     `);
+    assert.notOk(document.querySelector("[data-tippy-root]"));
   });
 
-  componentTest("it shows tooltip on mouseenter", {
-    template: hbs`
+  test("it shows tooltip on mouseenter", async function (assert) {
+    await render(hbs`
       <button>
         <DTooltip>
           Tooltip text
         </DTooltip>
       </button>
-    `,
+     `);
 
-    async test(assert) {
-      await mouseenter();
-      assert.ok(
-        document.querySelector("[data-tippy-root]"),
-        "the tooltip is added to the page"
-      );
-      assert.equal(
-        document
-          .querySelector("[data-tippy-root] .tippy-content")
-          .textContent.trim(),
-        "Tooltip text",
-        "the tooltip content is correct"
-      );
-    },
+    await mouseenter();
+    assert.ok(
+      document.querySelector("[data-tippy-root]"),
+      "the tooltip is added to the page"
+    );
+    assert.equal(
+      document
+        .querySelector("[data-tippy-root] .tippy-content")
+        .textContent.trim(),
+      "Tooltip text",
+      "the tooltip content is correct"
+    );
   });
 });
